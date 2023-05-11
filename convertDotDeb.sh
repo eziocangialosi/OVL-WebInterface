d4cb6fd7d4a9b8eb2efb81749def9c86d3ccc03e
Ezio Cangialosi <eziocangialosi@gmail.com>
2023-05-11 18:44:17 +0200

    Update package for debian 11 and earlier

--- convertDotDeb.sh
+++ convertDotDeb.sh
@@ -5,4 +5,4 @@ zstd -d < data.tar.zst | xz > data.tar.xz
 ar -m -c -a sdsd /tmp/ovl-website-deb11.deb debian-binary control.tar.xz data.tar.xz
 rm debian-binary control.tar.xz data.tar.xz control.tar.zst data.tar.zst
 cp /tmp/ovl-website-deb11.deb ./
-echo "Done \n"
+echo "Done"
